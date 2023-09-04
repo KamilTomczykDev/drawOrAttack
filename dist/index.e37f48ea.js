aa8786c57275fa4906a40682947cdc73c8261c75
Kamil Tomczyk <kamiltomczyk.dev@gmail.com>
2023-09-04 18:50:47 +0200

    mobile implementation 2/x :)

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -578,10 +578,17 @@ var _webImmediateJs = require("core-js/modules/web.immediate.js");
 var _runtime = require("regenerator-runtime/runtime");
 if (module.hot) module.hot.accept();
 ////////////////////////
-const elToHover = document.querySelector(".board--card--img");
-const secondEl = document.querySelector(".hover-view");
-elToHover.addEventListener("mouseover", ()=>secondEl.style.opacity = "1");
-elToHover.addEventListener("mouseout", ()=>secondEl.style.opacity = "0");
+const boardCard = document.querySelector(".board--card--img");
+const hoverView = document.querySelector(".hover-view");
+const enemySide = document.querySelector(".enemy-section");
+const enemyDisc = document.querySelector(".enemy-section--discription");
+const enemyHealth = document.querySelector(".enemy-section--hero--health");
+const createHover = function(firstEl, secondEl) {
+    firstEl.addEventListener("mouseover", ()=>secondEl.style.opacity = "1");
+    firstEl.addEventListener("mouseout", ()=>secondEl.style.opacity = "0");
+};
+createHover(boardCard, hoverView);
+createHover(enemySide, enemyDisc);
 
 },{"core-js/modules/web.immediate.js":"49tUX","regenerator-runtime/runtime":"dXNgZ"}],"49tUX":[function(require,module,exports) {
 "use strict";
--- src/js/controller.js
+++ src/js/controller.js
@@ -7,8 +7,16 @@ if (module.hot) {
 
 ////////////////////////
 
-const elToHover = document.querySelector(".board--card--img");
-const secondEl = document.querySelector(".hover-view");
+const boardCard = document.querySelector(".board--card--img");
+const hoverView = document.querySelector(".hover-view");
+const enemySide = document.querySelector(".enemy-section");
+const enemyDisc = document.querySelector(".enemy-section--discription");
+const enemyHealth = document.querySelector(".enemy-section--hero--health");
 
-elToHover.addEventListener("mouseover", () => (secondEl.style.opacity = "1"));
-elToHover.addEventListener("mouseout", () => (secondEl.style.opacity = "0"));
+const createHover = function (firstEl, secondEl) {
+  firstEl.addEventListener("mouseover", () => (secondEl.style.opacity = "1"));
+  firstEl.addEventListener("mouseout", () => (secondEl.style.opacity = "0"));
+};
+
+createHover(boardCard, hoverView);
+createHover(enemySide, enemyDisc);
